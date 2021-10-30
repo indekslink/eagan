0a6ae4c1439dd5bf562ba3910ca848f4d0757c75
riskiputraalamzah <aryainggil3@gmail.com>
2021-10-30 14:07:42 +0700

    push again

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,22 @@
-const hamburgerMenu = document.querySelector(".hamburger-menu");
+const hamburgerMenu = document.querySelectorAll(".hamburger-menu");
 const navbar = document.querySelector(".value-navbar");
-hamburgerMenu.addEventListener("click", function () {
-  ["show", "fixed"].map((e) => hamburgerMenu.classList.toggle(e));
+hamburgerMenu.forEach((hmb) =>
+  hmb.addEventListener("click", function () {
+    toggleMenu();
+  })
+);
+window.onscroll = () => {
+  const scr = window.scrollY;
+  [
+    document.querySelector(".to-top"),
+    document.querySelector(".navigation-bottom"),
+  ].map((e) => e.classList.toggle("show-navigation", scr > 50));
+};
+
+function toggleMenu() {
+  hamburgerMenu.forEach((hmb) => ["show"].map((e) => hmb.classList.toggle(e)));
   navbar.classList.toggle("show");
-});
+}
+document
+  .querySelector(".to-top")
+  .addEventListener("click", () => window.scrollTo(0, 0));
